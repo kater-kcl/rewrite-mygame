d48c6144c70da2ca35cee263022ea8baaf67947a
Kater-kcl <2276363693@qq.com>
2019-04-24 23:31:47 +0800

    Create tool.h

---
+++ rewriteofgame/tool.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  rewriteofgame
+//
+//  Created by xujin liu on 2019/4/24.
+//  Copyright © 2019 kater-KCl. All rights reserved.
+//
+
+#ifndef tool_h
+#define tool_h
+struct Ttool{
+    std::string name;
+    int num;
+    
+};
+#endif /* Header_h */
