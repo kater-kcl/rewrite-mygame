9bd7bf759cd0e77c290bb5fa1fd5a2404057805a
Kater-kcl <2276363693@qq.com>
2019-04-24 23:31:49 +0800

    Create mob.h

---
+++ rewriteofgame/mob.h
@@ -0,0 +1,18 @@
+//
+//  Header.h
+//  rewriteofgame
+//
+//  Created by xujin liu on 2019/4/23.
+//  Copyright © 2019 kater-KCl. All rights reserved.
+//
+
+#ifndef mob_h
+#define mob_h
+#include <string>
+struct tmob{
+    std::string name;
+    int hp;
+    int def;
+};
+
+#endif /* Header_h */
