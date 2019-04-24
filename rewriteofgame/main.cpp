c5ba634c44b401461dd56e4f4f53e01d8b269442
Kater-kcl <2276363693@qq.com>
2019-04-24 23:31:52 +0800

    Create main.cpp

---
+++ rewriteofgame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rewriteofgame
+//
+//  Created by xujin liu on 2019/4/23.
+//  Copyright © 2019 kater-KCl. All rights reserved.
+//
+
+#include <iostream>
+#include "mob.h"
+#include "tool.h"
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    return 0;
+}
